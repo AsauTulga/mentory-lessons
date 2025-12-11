21e6c2d7a0a9ed9e2b3c4c912108bd6a9f3fbf54
AsaTulga <asautulga@gmail.com>
2025-12-11 17:32:19 +0500

    last code js

--- dz js swiper/script.js
+++ dz js swiper/script.js
@@ -2,6 +2,22 @@ const swiper = new Swiper(".swiper", {
   slidesPerView: 4,
   loop: true,
   spaceBetween: 20,
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    640: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+    1400: {
+      slidesPerView: 4,
+    },
+  },
+
   pagination: {
     el: ".swiper-pagination",
     type: "bullets",
