5a7e5bc82874ced8b15c4dec57d0fc790cb9e494
AsaTulga <asautulga@gmail.com>
2025-12-18 10:04:05 +0500

    last dz

--- dz js glightbox & masonry/script.js
+++ dz js glightbox & masonry/script.js
@@ -1,15 +1,60 @@
 // Init Masonry
-var grid = document.querySelector(".grid");
+document.addEventListener("DOMContentLoaded", function () {
+  var grid = document.querySelector(".grid");
 
-var msnry = new Masonry(grid, {
-  itemSelector: ".grid-item",
-  originLeft: false,
-  originTop: false,
-});
+  var msnry = new Masonry(grid, {
+    itemSelector: ".grid-item",
+    gutter: 10,
+    percentPosition: true,
+  });
 
-// Init GLightbox
-const lightbox = GLightbox({
-  touchNavigation: true,
-  loop: true,
-  zoomable: true,
+  // Init GLightbox
+  const lightbox = GLightbox({
+    selector: ".glightbox",
+    touchNavigation: true,
+    loop: true,
+    plyr: {
+      css: "https://cdn.plyr.io/3.6.12/plyr.css",
+      js: "https://cdn.plyr.io/3.6.12/plyr.js",
+      config: {
+        ratio: "16:9",
+        fullscreen: {
+          enabled: true,
+          iosNative: true,
+        },
+        youtube: {
+          noCookie: true,
+          rel: 0,
+          showinfo: 0,
+          iv_load_policy: 3,
+        },
+        vimeo: {
+          byline: false,
+          portrait: false,
+          title: false,
+          transparent: false,
+        },
+      },
+    },
+  });
+
+  var images = document.querySelectorAll(".grid-item img");
+  var loadedImages = 0;
+
+  images.forEach(function (img) {
+    if (img.complete) {
+      loadedImages++;
+    } else {
+      img.addEventListener("load", function () {
+        loadedImages++;
+        if (loadedImages === images.length) {
+          msnry.layout();
+        }
+      });
+    }
+  });
+
+  if (loadedImages === images.length) {
+    msnry.layout();
+  }
 });
---
+++ lesson js 12/script.js
@@ -0,0 +1,14 @@
+import { SWIPER_CONFIG, GLIGHTBOX_CONFIG } from "./configs.js";
+import { getRandomNumber } from "./get-random-number.js";
+
+const heroCarousel = new Swiper(".hero-gallery", SWIPER_CONFIG);
+
+const lightbox = GLightbox(GLIGHTBOX_CONFIG);
+
+// console.log(getRandomNumber());
+
+// localStorage.setItem('theme', 'dark');
+
+// const myLocalUser = localStorage.getItem('theme');
+
+// console.log(myLocalUser);
